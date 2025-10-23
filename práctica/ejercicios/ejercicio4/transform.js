2dda08c4a8f7f2f806bc75d1fb2824f5976ee3b6
Sara Gallego Mendez <saragallegomendez10@gmail.com>
2025-10-23 02:54:00 +0200

    fix(exercise4): use CommonJS export and simplify project names with flatMap

--- práctica/ejercicios/ejercicio4/transform.js
+++ práctica/ejercicios/ejercicio4/transform.js
@@ -1,15 +1,11 @@
 // Lista de desarrolladores que tengan "JavaScript" en habilidades
 function desarrolladoresJavascript(datos) {
-    return datos.filter((dev) => dev.habilidades.includes('JavaScript'));
+  return datos.filter((dev) => dev.habilidades.includes('JavaScript'));
 }
-
 
 // Lista con los nombres de todos los proyectos
 function nombresProyectos(datos) {
-    return datos
-        .map((dev) => dev.proyectos)
-        .flat()
-        .map((p) => p.nombre);
+  return datos.flatMap((dev) => dev.proyectos.map((p) => p.nombre));
 }
 
-moduloReact.exports = { desarrolladoresJavascript, nombresProyectos };
+module.exports = { desarrolladoresJavascript, nombresProyectos };
