0973c60081d9f5b2fa58f6f2fcfdc02486fb1cff
Sara Gallego Mendez <saragallegomendez10@gmail.com>
2025-10-23 02:52:04 +0200

    fix(exercise3.3): return “<length> <reversed>” using length and template literal

--- práctica/ejercicios/ejercicio3/ejercicio3_3.js
+++ práctica/ejercicios/ejercicio3/ejercicio3_3.js
@@ -1,6 +1,6 @@
 function yourFunction(str) {
-    const reversed = str.split('').reverse().join('');
-    return '${str.lenght} ${reversed}';
+  const reversed = str.split('').reverse().join('');
+  return `${str.length} ${reversed}`;
 }
 
 module.exports = yourFunction;
