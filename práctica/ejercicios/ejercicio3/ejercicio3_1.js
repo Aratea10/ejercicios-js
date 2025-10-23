7c0fb422d5f8cc9da8e19aa33e697c1b47b9e305
Sara Gallego Mendez <saragallegomendez10@gmail.com>
2025-10-23 02:44:37 +0200

    fix(exercise3.1): use correct length and template literals to build file path

--- práctica/ejercicios/ejercicio3/ejercicio3_1.js
+++ práctica/ejercicios/ejercicio3/ejercicio3_1.js
@@ -1,8 +1,8 @@
 function yourFunction(arr) {
-    const ext = arr[arr.lenght - 1];
-    const file = arr[arr.lenght - 2];
-    const dirs = arr.slice(0, -2);
-    return [...dirs, '${file}.${ext}'].join('/');
+  const ext = arr[arr.length - 1];
+  const file = arr[arr.length - 2];
+  const dirs = arr.slice(0, -2);
+  return [...dirs, `${file}.${ext}`].join('/');
 }
 
 module.exports = yourFunction;
